92dac0e6cab93c7e9ddb338cafc4ed33b3dca2d1
Carlos Colman <ingcarloscolman@gmail.com>
2015-11-03 09:39:15 -0300

    Actulizacion 3-Nov

--- sites/all/themes/celtab/templates/page.tpl.php
+++ sites/all/themes/celtab/templates/page.tpl.php
@@ -165,7 +165,7 @@
         <?php print render($page['help']); ?>
         <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
         <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
+        <!--<?php print $feed_icons; ?> -->
       </section>
     </div>
     <!-- Page Content / End -->
