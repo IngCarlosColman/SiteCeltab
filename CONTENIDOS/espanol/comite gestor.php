7768efbc76e60c318e0b289bf5b9ff9967dae3bc
Carlos Colman <ingcarloscolman@gmail.com>
2015-11-17 16:10:17 -0300

    Gestion de traduccion del Sitio web, modulos y configuraciones

---
+++ CONTENIDOS/espanol/comite gestor.php
@@ -0,0 +1,80 @@
+
+<div class="sb-wrapper">
+	<!-- Headline -->
+	<h3 class="headline">Comite Gestor</h3>
+	<span class="line" style="margin-bottom: 20px;"></span>
+
+	<table border="0" style="width:100%">
+		<tr>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-01.jpg" alt="" /></td>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-02.jpg" alt="" /></td>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-03.jpg" alt="" /></td>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-04.jpg" alt="" /></td>
+		</tr>
+		<tr>
+			<td class="happy-clients-author" align="center">Marcos Siriaco</td>
+			<td class="happy-clients-author" align="center">Marcos Dellazari</td>
+			<td class="happy-clients-author" align="center">Filipe Leyser</td>
+			<td class="happy-clients-author" align="center">Sandro Martinez</td>
+		</tr>
+		<tr>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-05.jpg" alt="" /></td>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-06.jpg" alt="" /></td>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-07.jpg" alt="" /></td>
+			<td class="happy-clients-photo" align="center"><img src="/sites/all/themes/celtab/images/comite-08.jpg" alt="" /></td>
+		</tr>
+		<tr>
+			<td class="happy-clients-author" align="center">Paulo Carvalho</td>
+			<td class="happy-clients-author" align="center">Jonios Maximo</td>
+			<td class="happy-clients-author" align="center">Jose Washington de Medeiros</td>
+			<td class="happy-clients-author" align="center">João Paulo Barbosa</td>
+		</tr>
+	</table>
+	<div class="clearfix"></div>
+</div>
+
+
+
+//omitir este contenido
+<ul>
+		<li>	
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-01.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">Marcos Siriaco</div>
+		</li>
+		<li>
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-02.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">Marcos Dellazari</div>	
+		</li>
+		<li>
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-03.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">Filipe Leyser</div>
+		</li>
+		<li>
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-04.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">Sandro Martinez</div>
+		</li>
+		<li>
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-05.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">Paulo Carvalho</div>
+		</li>
+		<li>
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-06.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">Jonios Maximo</div>
+		</li>
+		<li>
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-07.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">Jose Washington de Medeiros</div>
+		</li>
+		<li>
+			<div class="happy-clients-photo"><img src="/sites/all/themes/celtab/images/comite-08.jpg" alt="" /></div>
+			<div class="happy-clients-cite"></div>
+			<div class="happy-clients-author">João Paulo Barbosa</div>
+		</li>
+	</ul>
